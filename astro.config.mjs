055439163c996d4a2487fbf4190efddc637efcc1
riolly <alberto.riolly@gmail.com>
2022-10-05 13:21:03 +0700

    :wrench: chore: image integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), solidJs()],
+  integrations: [tailwind(), solidJs(), image()]
 });
