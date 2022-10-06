67d43dfdf2ea28b9049ae2735a98278b3f3be194
riolly <alberto.riolly@gmail.com>
2022-10-06 23:02:36 +0700

    :recycle: refactor: eslint config

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -8,4 +8,18 @@ module.exports = {
 	jsxSingleQuote: true,
 	plugins: [require('prettier-plugin-tailwindcss')],
 	tailwindConfig: './tailwind.config.cjs',
+	overrides: [
+		{
+			files: ['.*', '*.json', '*.md', '*.toml', '*.yml'],
+			options: {
+				useTabs: false,
+			},
+		},
+		{
+			files: ['**/*.astro'],
+			options: {
+				parser: 'astro',
+			},
+		},
+	],
 }
