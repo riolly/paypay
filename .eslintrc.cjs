0dfea72f2bac095c6801c41d7da527d74ff8186f
riolly <alberto.riolly@gmail.com>
2022-10-06 10:33:45 +0700

    :recycle: refactor: format w/ prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,14 @@
 module.exports = {
-	extends: ["plugin:astro/recommended", "plugin:astro/jsx-a11y-recommended"],
+	extends: ['plugin:astro/recommended', 'plugin:astro/jsx-a11y-recommended'],
 	overrides: [
 		{
-			files: ["*.astro"],
-			parser: "astro-eslint-parser",
+			files: ['*.astro'],
+			parser: 'astro-eslint-parser',
 			parserOptions: {
-				parser: "@typescript-eslint/parser",
-				extraFileExtensions: [".astro"],
+				parser: '@typescript-eslint/parser',
+				extraFileExtensions: ['.astro'],
 			},
 			rules: {},
 		},
 	],
-};
+}
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from "astro/config";
-import solidJs from "@astrojs/solid-js";
-import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
+import {defineConfig} from 'astro/config'
+import solidJs from '@astrojs/solid-js'
+import tailwind from '@astrojs/tailwind'
+import image from '@astrojs/image'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs(), image()]
-});
+	integrations: [tailwind(), solidJs(), image()],
+})
