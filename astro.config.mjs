1af54359292f2b3bae4fdb0065415108b831b054
riolly <alberto.riolly@gmail.com>
2022-10-07 09:42:01 +0700

    :wrench: chore: disable ssr & sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,18 @@ import {defineConfig} from 'astro/config'
 import solidJs from '@astrojs/solid-js'
 import tailwind from '@astrojs/tailwind'
 import image from '@astrojs/image'
+// import netlify from '@astrojs/netlify/functions'
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		tailwind(),
 		solidJs(),
-		image({
-			serviceEntryPoint: '@astrojs/image/sharp',
-		}),
+		image(),
+		// image({
+		// 	serviceEntryPoint: '@astrojs/image/sharp',
+		// }),
 	],
+	output: 'static',
+	// adapter: netlify(),
 })
